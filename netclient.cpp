c12853a46920c857cc000d3cf3fd43ceb00fd43b
Cheng <chenglv2007@gmail.com>
2013-03-10 01:38:09 +0000

    netclient added

---
+++ netclient.cpp
@@ -0,0 +1,12 @@
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+int main(int argc,char ** argv)
+{
+
+	return 0;
+}
+
